e9f86f55e0f0c226375e69b2be17d58638069e52
elknu <elknumber1@yandex.com>
2026-03-28 23:02:15 +0300

    Task 2.3 is completed

--- 02-basics/2.3-collections/tasks/task02/main.go
+++ 02-basics/2.3-collections/tasks/task02/main.go
@@ -19,16 +19,29 @@ package main
 
 import (
 	"fmt"
+	"maps"
 	"slices"
 )
 
 // TODO: напиши функцию invertMap(m map[string]int) map[int]string
+
+func invertMap(m map[string]int) map[int]string {
+	resultMap := make(map[int]string)
+
+	for key, value := range m {
+		resultMap[value] = key
+	}
+	return resultMap
+}
 
 func main() {
 	fruits := map[string]int{
 		"яблоко":   1,
 		"банан":    2,
 		"апельсин": 3,
+		"груша":    10,
+		"киви":     5,
+		"шоколад":  2,
 	}
 
 	// TODO: вызови invertMap и сохрани результат
@@ -37,6 +50,14 @@ func main() {
 	// TODO: собери ключи из inverted в срез, отсортируй их
 	// и выведи каждую пару в формате: "1 -> яблоко"
 
-	_ = fruits
-	_ = slices.Sort[[]int] // убери когда будешь использовать
+	inverted := invertMap(fruits)
+	resultSlice := slices.Collect(maps.Keys(inverted))
+	slices.Sort(resultSlice)
+
+	for _, element := range resultSlice {
+		fmt.Printf("%d -> %s\n", element, inverted[element])
+	}
+
+	// Забавно, в результате случайным образом могут по ключу 2 вернуться и банан, и шоколад, так как в методе invertMap
+	// пары значений берутся рандомно. И в зависимости от того, что придет вторым, то и останется в инвертированной мапе
 }
